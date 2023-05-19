b5c54cf8ce460abae93e1b35d42588db705d91c3
Sai_Shellikeri <85837747+sai7350@users.noreply.github.com>
2023-05-19 15:17:35 +0530

    Update shaniwarWada.vue

--- pages/shaniwarWada.vue
+++ pages/shaniwarWada.vue
@@ -46,7 +46,7 @@
             </div>
         </div>
         <div class="sticky bottom-5 pb-10">
-            <div class="mx-auto bg-black rounded-full md:w-3/4 ">
+            <div class="mx-auto bg-black rounded-full md:w-3/4 px-2 ">
                 <audio controls class="mx-auto w-full">
                     <source src="../assets/ShaniwarWadaImages/Audio.mp3">
                 </audio>
