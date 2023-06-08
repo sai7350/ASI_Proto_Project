7c51bc96bdb9cdb3f96ca0697fe7ff92f3bde53a
Sai_Shellikeri <85837747+sai7350@users.noreply.github.com>
2023-06-08 17:33:45 +0530

    Update Map.vue

--- pages/Map.vue
+++ pages/Map.vue
@@ -3,7 +3,7 @@
 
     <head>
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" /> 
     </head>
   </div>
   <!-- <div style="height: 100vh;" id="map"></div> -->
